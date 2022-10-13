2657b05ffbc21fb5f8ec7a4c0107fd1c03859484
Progress <ezeamakachisom2002@gmail.com>
2022-10-13 16:50:15 -0700

    Add eventlisterner to window

--- script.js
+++ script.js
@@ -386,12 +386,20 @@ const contactEmailField = document.getElementById('email');
 const contactUsernameField = document.getElementById('name');
 const contactMessageField = document.getElementById('textarea');
 
-
-
 contactForm.addEventListener('input', () => {
   const username = contactUsernameField.value;
   const email = contactEmailField.value;
   const message = contactMessageField.value;
 
   localStorage.setItem('contact-form', JSON.stringify({ username, email, message }));
+});
+
+window.addEventListener('load', () => {
+  const isDataExist = JSON.parse(localStorage.getItem('contact-form'));
+
+  if (!isDataExist) return;
+
+  contactUsernameField.value = isDataExist.username;
+  contactEmailField.value = isDataExist.email;
+  contactMessageField.value = isDataExist.message;
 });
