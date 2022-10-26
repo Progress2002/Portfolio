bf93754be42f59d8fa20b3af29319168e58fda84
Progress <ezeamakachisom2002@gmail.com>
2022-10-25 21:42:29 -0700

    Modify project.js file

--- projects.js
+++ projects.js
@@ -142,126 +142,126 @@ const projects = {
 
 // Work section----------------
 const workSection = document.querySelector('#work-section');
-const work = document.createElement('div');
-work.setAttribute('id', 'work');
-workSection.appendChild(work);
 
-work.innerHTML = `
-              <div class="work-title">
-                  <div class="text">
-                      <h2>My Recent Works</h2>
-                  </div>
-                  <div class="horizontal-line">
-                      <hr>
-                  </div>
-              </div>
-              <ul id="work-content">
-                  <li class="project pr-1">
-                      <div class="image">
-                          <img class="img-mobile" src="${projects.one.featured_image.mobile}" alt="Project-img">
-                          <img class="img-desktop" src="${projects.one.featured_image.desktop}" alt="project-img">
-                      </div>
-                      <div class="body">
-                          <div class="body-content">
-                              <h3>${projects.one.title}</h3>
-                              <p>${projects.one.description}</p>
-                              <ul>
-                                  <li>${projects.one.technologies.one}</li>
-                                  <li>${projects.one.technologies.two}</li>
-                                  <li>${projects.one.technologies.three}</li>
-                                  <li>${projects.one.technologies.four}</li>
-                              </ul>
-                          </div>
-                          <div class="btn">
-                              <a href="#1" class="button popup-btn" id="one">See Project</a>
-                          </div>
-                      </div>
-                  </li>
-                  <li class="project pr-2 bg-img btn-desktop">
-                      <div class="body-content">
-                          <h3>${projects.two.title}</h3>
-                          <p>${projects.two.description1}</p>
-                          <ul>
-                              <li>${projects.two.technologies.one}</li>
-                              <li>${projects.two.technologies.two}</li>
-                              <li>${projects.two.technologies.three}</li>
-                          </ul>
-                      </div>
-                      <div class="btn">
-                          <a href="#2" class="button popup-btn" id="two">See Project</a>
-                      </div>
-                  </li>
-                  <li class="project pr-3 bg-img btn-desktop">
-                      <div class="body-content">
-                          <h3>${projects.three.title}</h3>
-                          <p>${projects.three.description1}</p>
-                          <ul>
-                              <li>${projects.three.technologies.one}</li>
-                              <li>${projects.three.technologies.two}</li>
-                              <li>${projects.three.technologies.three}</li>
-                          </ul>
-                      </div>
-                      <div class="btn">
-                          <a href="#3" class="button popup-btn" id="three">See Project</a>
-                      </div>
-                  </li>
-                  <li class="project pr-4 bg-img btn-desktop">
-                      <div class="body-content">
-                          <h3>${projects.four.title}</h3>
-                          <p>${projects.four.description1}</p>
-                          <ul>
-                              <li>${projects.four.technologies.one}</li>
-                              <li>${projects.four.technologies.two}</li>
-                              <li>${projects.four.technologies.three}</li>
-                          </ul>
-                      </div>
-                      <div class="btn">
-                          <a href="#4" class="button popup-btn" id="four">See Project</a>
-                      </div>
-                  </li>
-                  <li class="project pr-5 bg-img btn-desktop">
-                      <div class="body-content">
-                          <h3>${projects.five.title}</h3>
-                          <p>${projects.five.description1}</p>
-                          <ul>
-                              <li>${projects.five.technologies.one}</li>
-                              <li>${projects.five.technologies.two}</li>
-                              <li>${projects.five.technologies.three}</li>
-                          </ul>
-                      </div>
-                      <div class="btn">
-                          <a href="#5" class="button popup-btn" id="five">See Project</a>
-                      </div>
-                  </li>
-                  <li class="project pr-6 bg-img btn-desktop">
-                      <div class="body-content">
-                          <h3>${projects.six.title}</h3>
-                          <p>${projects.six.description1}</p>
-                          <ul>
-                              <li>${projects.six.technologies.one}</li>
-                              <li>${projects.six.technologies.two}</li>
-                              <li>${projects.six.technologies.three}</li>
-                          </ul>
-                      </div>
-                      <div class="btn">
-                          <a href="#6" class="button popup-btn" id="six">See Project</a>
-                      </div>
-                  </li>
-                  <li class="project pr-7 bg-img btn-desktop">
-                      <div class="body-content">
-                          <h3>${projects.seven.title}</h3>
-                          <p>${projects.seven.description1}</p>
-                          <ul>
-                              <li>${projects.seven.technologies.one}</li>
-                              <li>${projects.seven.technologies.two}</li>
-                              <li>${projects.seven.technologies.three}</li>
-                          </ul>
-                      </div>
-                      <div class="btn">
-                          <a href="#7" class="button popup-btn" id="seveen" >See Project</a>
-                      </div>
-                  </li>
-              </ul>`;
+workSection.innerHTML += `
+  <div id="work">
+  <div class="work-title">
+  <div class="text">
+      <h2>My Recent Works</h2>
+  </div>
+  <div class="horizontal-line">
+      <hr>
+  </div>
+</div>
+<ul id="work-content">
+  <li class="project pr-1">
+      <div class="image">
+          <img class="img-mobile" src="${projects.one.featured_image.mobile}" alt="Project-img">
+          <img class="img-desktop" src="${projects.one.featured_image.desktop}" alt="project-img">
+      </div>
+      <div class="body">
+          <div class="body-content">
+              <h3>${projects.one.title}</h3>
+              <p>${projects.one.description}</p>
+              <ul>
+                  <li>${projects.one.technologies.one}</li>
+                  <li>${projects.one.technologies.two}</li>
+                  <li>${projects.one.technologies.three}</li>
+                  <li>${projects.one.technologies.four}</li>
+              </ul>
+          </div>
+          <div class="btn">
+              <a href="#1" class="button popup-btn" id="one">See Project</a>
+          </div>
+      </div>
+  </li>
+  <li class="project pr-2 bg-img btn-desktop">
+      <div class="body-content">
+          <h3>${projects.two.title}</h3>
+          <p>${projects.two.description1}</p>
+          <ul>
+              <li>${projects.two.technologies.one}</li>
+              <li>${projects.two.technologies.two}</li>
+              <li>${projects.two.technologies.three}</li>
+          </ul>
+      </div>
+      <div class="btn">
+          <a href="#2" class="button popup-btn" id="two">See Project</a>
+      </div>
+  </li>
+  <li class="project pr-3 bg-img btn-desktop">
+      <div class="body-content">
+          <h3>${projects.three.title}</h3>
+          <p>${projects.three.description1}</p>
+          <ul>
+              <li>${projects.three.technologies.one}</li>
+              <li>${projects.three.technologies.two}</li>
+              <li>${projects.three.technologies.three}</li>
+          </ul>
+      </div>
+      <div class="btn">
+          <a href="#3" class="button popup-btn" id="three">See Project</a>
+      </div>
+  </li>
+  <li class="project pr-4 bg-img btn-desktop">
+      <div class="body-content">
+          <h3>${projects.four.title}</h3>
+          <p>${projects.four.description1}</p>
+          <ul>
+              <li>${projects.four.technologies.one}</li>
+              <li>${projects.four.technologies.two}</li>
+              <li>${projects.four.technologies.three}</li>
+          </ul>
+      </div>
+      <div class="btn">
+          <a href="#4" class="button popup-btn" id="four">See Project</a>
+      </div>
+  </li>
+  <li class="project pr-5 bg-img btn-desktop">
+      <div class="body-content">
+          <h3>${projects.five.title}</h3>
+          <p>${projects.five.description1}</p>
+          <ul>
+              <li>${projects.five.technologies.one}</li>
+              <li>${projects.five.technologies.two}</li>
+              <li>${projects.five.technologies.three}</li>
+          </ul>
+      </div>
+      <div class="btn">
+          <a href="#5" class="button popup-btn" id="five">See Project</a>
+      </div>
+  </li>
+  <li class="project pr-6 bg-img btn-desktop">
+      <div class="body-content">
+          <h3>${projects.six.title}</h3>
+          <p>${projects.six.description1}</p>
+          <ul>
+              <li>${projects.six.technologies.one}</li>
+              <li>${projects.six.technologies.two}</li>
+              <li>${projects.six.technologies.three}</li>
+          </ul>
+      </div>
+      <div class="btn">
+          <a href="#6" class="button popup-btn" id="six">See Project</a>
+      </div>
+  </li>
+  <li class="project pr-7 bg-img btn-desktop">
+      <div class="body-content">
+          <h3>${projects.seven.title}</h3>
+          <p>${projects.seven.description1}</p>
+          <ul>
+              <li>${projects.seven.technologies.one}</li>
+              <li>${projects.seven.technologies.two}</li>
+              <li>${projects.seven.technologies.three}</li>
+          </ul>
+      </div>
+      <div class="btn">
+          <a href="#7" class="button popup-btn" id="seveen" >See Project</a>
+      </div>
+  </li>
+</ul>
+  </div>
+`;
 
 // POPUP SECTION------------------------
 const popupBtn = document.querySelectorAll('.popup-btn');
@@ -273,11 +273,8 @@ function openPopup() {
   overlay.classList.add('active');
 }
 
-const popupContainer = document.createElement('div');
-popupContainer.classList.add('popup-container');
-popup.appendChild(popupContainer);
-
-popupContainer.innerHTML = `
+popup.innerHTML += `
+    <div class="popup-container">
               <div class="popup-header">
                   <h2 class="popup-title">Multi Post Stories</h2>
                   <button class="close-popup">&times;</button>
@@ -302,6 +299,7 @@ popupContainer.innerHTML = `
                       </div>
                   </div>
               </div>
+    </div>
   `;
 const closePopup = document.querySelector('.close-popup');
 
