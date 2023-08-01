27ea01f76039c075c517d844989dde43aa069a1e
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-08-01 22:22:45 +0100

    Update projectData.js

--- module/projectData.js
+++ module/projectData.js
@@ -22,19 +22,19 @@ export const projects = {
     },
   },
   two: {
-    title: 'Profesional Art Printing Data',
+    title: 'CarBooker',
     description:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+      "Cabooker is a user-friendly and efficient car booking application that empowers users to reserve their preferred vehicles seamlessly. Whether you need a car for a business trip, vacation, or simply for your daily commute, Cabooker offers a diverse selection of cars to suit your needs.",
     description1:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+      "Cabooker is a user-friendly and efficient car booking application that empowers users to reserve their preferred vehicles seamlessly. Whether you need a car for a business trip, vacation, or simply for your daily commute, Cabooker offers a diverse selection of cars to suit your needs.",
     featured_image: {
-      mobile: './images/Img Placeholder (2).png',
-      desktop: './images/Img Placeholder (2).png',
+      mobile: './images/Carbooker.png',
+      desktop: './images/Carbooker.png',
     },
     technologies: [
-      'html',
-      'bootstrap',
-      'Ruby',
+      'React/Redux',
+      'CSS3',
+      'Ruby on Rails',
     ],
     links: {
       live: '#',
@@ -42,66 +42,87 @@ export const projects = {
     },
   },
   three: {
-    title: 'Profesional Art Printing Data',
+    title: '🌏Countries',
     description:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+      "In this web application, users are able to see all the countries in the world on the homepage, filter countries by region, click on a country to see more information, and search for a country.",
     description1:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+      "In this web application, users are able to see all the countries in the world on the homepage, filter countries by region, click on a country to see more information, and search for a country.",
     featured_image: {
-      mobile: './images/Img Placeholder (2).png',
-      desktop: './images/Img Placeholder (2).png',
+      mobile: './images/countries.png',
+      desktop: './images/countries.png',
     },
     technologies: [
-      'html',
-      'bootstrap',
-      'Ruby',
+      'React/Redux',
+      'CSS3',
+      'HTML5',
     ],
     links: {
-      live: '#',
-      source_code: '#',
+      live: 'https://countires.onrender.com/',
+      source_code: 'https://github.com/Progress2002/Country.git',
     },
   },
   four: {
-    title: 'Profesional Art Printing Data',
+    title: 'Multi-step form',
     description:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+      "This is a solution to the Multi-step form challenge on Frontend Mentor. It verifies users' input and grants them access to a new form page where they could purchase different plans, after which they are provided with a summary of all their selections and could confirm the purchase or make changes.",
     description1:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+      "A signup form that verifies users' input and grants them access to multiple choice of item subscription and a preview of all selection made.",
     featured_image: {
-      mobile: './images/Img Placeholder (2).png',
-      desktop: './images/Img Placeholder (2).png',
+      mobile: './images/multi-step\ form.png',
+      desktop: './images/multi-step\ form.png',
     },
     technologies: [
-      'html',
-      'bootstrap',
-      'Ruby',
+      'HTML',
+      'SCSS',
+      'JavaScript',
     ],
     links: {
-      live: '#',
-      source_code: '#',
+      live: 'https://progress2002.github.io/Multi-step-form-/',
+      source_code: 'https://github.com/Progress2002/Multi-step-form-.git',
     },
   },
   five: {
-    title: 'Profesional Art Printing Data',
+    title: 'Keeper',
     description:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+      "This is a React application that allows users to add new notes by providing the title and text of the note. It also displays the date and time at which the note was taken.",
     description1:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+      "This is a React application that allows users to add new notes by providing the title and text of the note. It also displays the date and time at which the note was taken.",
     featured_image: {
-      mobile: './images/Img Placeholder (2).png',
-      desktop: './images/Img Placeholder (2).png',
+      mobile: './images/Keeper.png',
+      desktop: './images/Keeper.png',
     },
     technologies: [
-      'html',
-      'bootstrap',
-      'Ruby',
+      'React',
+      'CSS3',
+      'JavaScript'
     ],
     links: {
-      live: '#',
-      source_code: '#',
+      live: 'https://keeper-v8lq.onrender.com/',
+      source_code: 'https://github.com/Progress2002/Keeper.git',
     },
   },
   six: {
+    title: 'Easybank landing page',
+    description:
+      "This is a solution to the Easybank landing page challenge on Frontend Mentor Frontend Mentor challenges help you improve your coding skills by building realistic projects.",
+    description1:
+    "This is a solution to the Easybank landing page challenge on Frontend Mentor Frontend Mentor challenges help you improve your coding skills by building realistic projects.",
+    featured_image: {
+      mobile: './images/easybank.png',
+      desktop: './images/easybank.png',
+    },
+    technologies: [
+      'React',
+      'Tailwind CSS',
+      'React-reveal',
+      'JavaScript'
+    ],
+    links: {
+      live: 'https://easybank-spt9.onrender.com/',
+      source_code: 'https://github.com/Progress2002/easybank.git',
+    },
+  },
+  seven: {
     title: 'To Do App',
     description:
     "A To-Do list application that allows users to keep record of activity they intend doing. This app allows users to mark activites as completed and also edith alredy assigned activitie.",
@@ -120,26 +141,6 @@ export const projects = {
     links: {
       live: 'https://progress2002.github.io/To-Do-List/dist/',
       source_code: 'https://github.com/Progress2002/To-Do-List.git',
-    },
-  },
-  seven: {
-    title: 'Multi-step form',
-    description:
-      "This is a solution to the Multi-step form challenge on Frontend Mentor. It verifies users' input and grants them access to a new form page where they could purchase different plans, after which they are provided with a summary of all their selections and could confirm the purchase or make changes.",
-    description1:
-      "A signup form that verifies users' input and grants them access to multiple choice of item subscription and a preview of all selection made.",
-    featured_image: {
-      mobile: './images/multi-step\ form.png',
-      desktop: './images/multi-step\ form.png',
-    },
-    technologies: [
-      'HTML',
-      'SCSS',
-      'JavaScript',
-    ],
-    links: {
-      live: 'https://progress2002.github.io/Multi-step-form-/',
-      source_code: 'https://github.com/Progress2002/Multi-step-form-.git',
     },
   },
 };
